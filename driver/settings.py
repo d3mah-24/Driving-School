d97fe148995d633086023e631c11fc5c4bb8dbd6
d3mah-24 <64283607+d3mah-24@users.noreply.github.com>
2022-12-04 16:27:09 +0300

    Update settings.py

--- driver/settings.py
+++ driver/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os.path
+import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fo+2h$a&2yfy2+i-o6f*6hzzfe&o9&lf1r#909&=ud9$4fu%s='
+SECRET_KEY =  os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =False
